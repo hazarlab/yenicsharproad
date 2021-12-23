724d03352f1c371c492f815ad5a6a95bf525da7b
hazar <hazarakgul@gmail.com>
2021-12-23 10:46:09 +0300

    c sharp ogrenme sureci
    bu surecte hazırlanan ve uzerinde calisilan basit projeler

---
+++ newamq/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("İsminizi giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soy isminizi giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba, " + name + " " + surname);
